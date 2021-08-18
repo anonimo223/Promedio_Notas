a6cd28aa4821d7ab34558c5671bb1b6aba9227e0
anonimo223 <anguielugo44@gmail.com>
2021-08-18 09:59:33 -0500

    Esquema: Esquema inicial del programa para calcular el promedio

---
+++ promedi.py
@@ -0,0 +1,14 @@
+print("Calcula el Promedio UwU")
+
+n1 = float(input("Ingresa N1--> "))
+n2 = float(input("Ingresa N2--> "))
+n3 = float(input("Ingresa N3--> "))
+n4 = float(input("Ingresa N4--> "))
+n5 = float(input("Ingresa N5--> "))
+
+
+prom = (n1+n2+n3+n4+n5)/3
+if(prom>=3):
+    print("Su promedio es: ",prom,"Felicidades gano la materia UwU")
+else:
+    print("Su promedio es: ",prom,"Lo siento, pero perdio la AMteria :'C")
